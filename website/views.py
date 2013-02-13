0c6d6ddc3998528a11cecfeebf77dd99cca344bc
Leonardo Ferreyra <leferreyra@gmail.com>
2013-02-12 23:17:42 -0300

    Agrega estilos base y estructura del sitio

--- website/views.py
+++ website/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+# Vista provisoria del index
+def index(request):
+
+	return render_to_response(
+		'main.html', {},
+		context_instance=RequestContext(request));
